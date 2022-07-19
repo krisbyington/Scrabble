4f6850b1415e2ee7193a7543b398b3c7038241a2
krisbyington <krisbyington@gmail.com>
2022-07-19 14:12:06 -0700

    removed the add by room code option

--- public/javascript/browseLobby.js
+++ public/javascript/browseLobby.js
@@ -3,6 +3,7 @@ const lobby_id = document.getElementById('lobby-id');
 document.querySelectorAll('.button-section').forEach(item => {
     item.addEventListener('click', async function () {
         const x = (item.parentNode);
+        console.log(item);
         let lobbyId = String(x.childNodes[1].textContent).trim();
         window.location.href = `/lobby/${lobbyId}`, true;
     })
