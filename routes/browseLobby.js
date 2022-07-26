919979f89f7eacf051f35e450849b9e5ef843de9
krisbyington <43834863+krisbyington@users.noreply.github.com>
2022-07-26 13:28:49 -0700

    Update browseLobby.js

--- routes/browseLobby.js
+++ routes/browseLobby.js
@@ -1,31 +1,52 @@
-const e = require("express");
 const express = require("express");
 const router = express.Router();
 const db = require('../db');
 const Game = require("../db/game");
 
-router.get("/", (request, response) => {
-    Game.getGames().then((games) => {
-        response.render('browseLobby', {
-            style: 'style',
-            lobbies: games
-        });
-    })
-        .catch((err) => Promise.reject(err));
+router.get("/", async (request, response) => {
+    let allGameData =  await Game.getAllGameInfo();
+    let games = await Game.getGames();
+    let clientData = [];
+    for(game of games ){
+        playerList = [];
+        for(data of allGameData){
+            if(game.id == data.game_id){
+                playerList.push(data.username);
+            }
+        }
+        if(game.in_lobby){
+            clientData.push({
+                id: game.id,
+                players: playerList
+            });
+        }
+    }
+    response.render('browseLobby', {
+        browseLobby: true,
+        style: 'browseLobbyStyle',
+        lobbies: clientData
+    });
 });
 
-router.get("/leave/:id", (request, response) => {
-    if (request.session) {
-        let userId = request.session.user_id;
-        let gameId = request.params.id;
-        Game.removeFromLobby(gameId, userId)
-            .then(() => {
-                response.redirect("/browseLobby");
-            })
-    } else {
-        console.log("NO SESSION");
+router.get("/refresh", async (request , response) =>{
+    console.log("in refresh")
+    let allGameData =  await Game.getAllGameInfo();
+    let games = await Game.getGames();
+    let clientData = [];
+    for( game of games ){
+        playerList = [];
+        for(data of allGameData){
+            if(game.id == data.game_id){
+                playerList.push(data.username);
+            }
+        }
+        clientData.push({
+            id: game.id,
+            in_lobby: game.in_lobby,
+            players: playerList
+        });
     }
-    Game.removeFromLobby(request.session.id)
-})
+    response.set("Content-Type", "application/json").send({clientData});
+});
 
 module.exports = router;
