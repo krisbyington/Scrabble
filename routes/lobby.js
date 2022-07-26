0de8284237646ca4ba339f56f22976a9db1f141b
krisbyington <43834863+krisbyington@users.noreply.github.com>
2022-07-26 13:29:32 -0700

    Update lobby.js

--- routes/lobby.js
+++ routes/lobby.js
@@ -18,9 +18,12 @@ router.get("/", (request, response) => {
 router.get("/:id", async (request, response) => {
   var gameID = request.params.id;
   var userID = request.session.user_id;
+  var username;
   var gameUsers;
   var gameData;
   var highestOrder = 0;
+
+  username = await game.getUserNameFromId(userID);
   gameData = await game.getGameById(gameID);
   if(gameData.in_lobby == true){
     gameUsers = await game.getGameUsers2(gameID);
@@ -38,15 +41,18 @@ router.get("/:id", async (request, response) => {
     await game.joinGame(gameID, userID);
     await game.updateGameUserOrder(gameID, userID, highestOrder + 1);
     gameUsers = await game.getGameUsers2(gameID);
-
+    request.app.get("io").sockets.to("lobby" + gameID).emit("playerJoined", {username});
     response.render("lobby", {
+      lobby: true,
       style: 'lobbyStyle',
       players: gameUsers,
       currUser: userID,
       gameId: gameID
     })
   }else{
+    request.app.get("io").sockets.to("lobby" + gameID).emit("playerJoined", {username});
     response.render("lobby", {
+      lobby: true,
       style: 'lobbyStyle',
       players: gameUsers,
       currUser: userID,
@@ -55,32 +61,40 @@ router.get("/:id", async (request, response) => {
   }
 })
 
-router.get("/leave/:id", (request, response) => {
+router.get("/leave/:id", async (request, response) => {
   if (request.session) {
     let userId = request.session.user_id;
     let gameId = request.params.id;
+    username = await game.getUserNameFromId(userId);
+    request.app.get("io").sockets.to("lobby" + gameId).emit("playerLeft", {username});
     game.removeFromLobby(gameId, userId)
       .then(() => {
         response.redirect("/browseLobby");
       })
   } else {
     console.log("NO SESSION");
+    response.sendStatus(403);
   }
 })
 
 router.use("/startGame/:id", async (request, response) => {
-  let GameUsers = await game.getGameUsers2(request.body.Id);
+  let gameUsers = await game.getGameUsers2(request.params.id);
   let allReady = false;
-  // for(user in gameUsers){
-  //   if(user.is_ready == true){
-  //     allReady = true;
-  //   }else{
-  //     allReady = false;
-  //     break;
-  //   }
-  // }
-  await game.startGame(request.params.id);
-  response.redirect(`/game/${request.params.id}`);
+  for(user of gameUsers){
+    if(user.is_ready == true){
+      allReady = true;
+    }else{
+      allReady = false;
+      break;
+    }
+  }
+  console.log("allReady", allReady)
+  if(allReady){
+    await game.startGame(request.params.id);
+    request.app.get("io").sockets.to("lobby" + request.params.id).emit("startGame",{route: `/game/${request.params.id}`});
+  }else{
+    return response.sendStatus(200);
+  }
 });
 
 router.use("/ready/:id", async (request, response) => {
@@ -91,8 +105,9 @@ router.use("/ready/:id", async (request, response) => {
   if(isReady){
     username = await game.getUserNameFromId(userId);
     request.app.get("io").sockets.to("lobby" + request.params.id).emit("madeReady", {username});
-    console.log(username);
+    return response.sendStatus(200);
   }
+  return response.sendStatus(500);
 });
 
 router.use("/unready/:id", async (request, response) => {
@@ -103,16 +118,9 @@ router.use("/unready/:id", async (request, response) => {
   if(!isReady){
     username = await game.getUserNameFromId(userId);
     request.app.get("io").sockets.to("lobby" + request.params.id).emit("madeUnready", {username});
-    console.log(username);
+    return response.sendStatus(200);
   }
-  // .then(results => {
-  //   console.log("db returning", results[0].is_ready);
-  //   if(!results[0].is_ready){
-  //     username = game.getUserNameFromId(userId);
-  //     console.log(username);
-  //     request.app.get("io").sockets.to("lobby" + request.params.id).emit("madeUnready");
-  //   }
-  // });
+  return response.sendStatus(500);
 })
 
 module.exports = router;
