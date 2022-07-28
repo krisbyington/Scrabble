a5ede3a9e1dee4a0fa06bb95a279d868c979a3a0
krisbyington <krisbyington@gmail.com>
2022-07-28 16:56:15 -0700

    db config

--- config/config.js
+++ config/config.js
@@ -13,7 +13,7 @@ module.exports = {
     use_env_variable: "DATABASE_URL",
     dialect: "postgres",
    dialectOptions: {
-    ssl: {require: true},
+    ssl: { rejectUnauthorized: false},
  }
   },
 };
