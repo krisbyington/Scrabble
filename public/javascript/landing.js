3383ce83edb618cbec03fd1c2842d30c201c24ff
Kris <krisbyington@gmail.com>
2022-07-30 17:27:59 -0700

    added sliders that pop in and out with button press

--- public/javascript/landing.js
+++ public/javascript/landing.js
@@ -1,43 +1,180 @@
-const container = document.getElementById("login-buttons-container")
+const container = document.getElementById("login-buttons-container");
+var registerDiv = container.children[0];
+const loginDiv = container.children[1];
+console.log(container.children[0])
 console.log(container.children)
-const loginSlider = container.children[1];
-const registrationSlider = container.children[4];
-const loginButton = container.children[2];
-const registerButton = container.children[5]
-console.log(loginButton)
-console.log(registerButton)
-console.log(loginSlider)
-console.log(registrationSlider)
- //need to build the HTML internals based on the partial and just add it
-  //need to build the HTML internals based on the partial and just add it
+const registerButton = container.children[0].children[0];
+const loginButton = container.children[1].children[0];
+
 
 var loginButtonPressed = false;
 var registerButtonPressed = false;
 
-const openRegistration = () => {
+var registrationSlider = document.createElement("div");
+registrationSlider.className = "slider-container";
+registrationSlider.id = "registration-slider";
+let tempDiv = document.createElement("div");
+tempDiv.classList.add("auth-container");
+registrationSlider.append(tempDiv);
+//add username input field 
+tempDiv = document.createElement("div");
+tempDiv.className = "input-field";
+let tempLabel = document.createElement("label");
+tempLabel.className = "input-name";
+tempLabel.innerText = "Username";
+let tempInput = document.createElement("input");
+tempInput.className = "form-input";
+tempInput.name = "register-username";
+tempInput.type = "text";
+tempDiv.append(tempLabel);
+tempDiv.append(tempInput);
+registrationSlider.append(tempDiv);
+//add password input field\
+tempDiv = document.createElement("div");
+tempDiv.className = "input-field";
+tempLabel = document.createElement("label");
+tempLabel.className = "input-name";
+tempLabel.innerText = "Password";
+tempInput = document.createElement("input");
+tempInput.className = "form-input";
+tempInput.name = "register-password";
+tempInput.type = "text";
+tempDiv.append(tempLabel);
+tempDiv.append(tempInput);
+registrationSlider.append(tempDiv);
+//add confirm password input
+tempDiv = document.createElement("div");
+tempDiv.className = "input-field";
+tempLabel = document.createElement("label");
+tempLabel.className = "input-name";
+tempLabel.innerText = "Confirm Password";
+tempInput = document.createElement("input");
+tempInput.className = "form-input";
+tempInput.name = "register-confirm-password";
+tempInput.type = "text";
+tempDiv.append(tempLabel);
+tempDiv.append(tempInput);
+registrationSlider.append(tempDiv);
+
+var loginSlider = document.createElement("div");
+loginSlider.className = "slider-container";
+loginSlider.id = "login-slider";
+tempDiv = document.createElement("div");
+tempDiv.classList.add("auth-container");
+loginSlider.append(tempDiv);
+
+//add username input field 
+tempDiv = document.createElement("div");
+tempDiv.className = "input-field";
+tempLabel = document.createElement("label");
+tempLabel.className = "input-name";
+tempLabel.innerText = "Username";
+tempInput = document.createElement("input");
+tempInput.className = "form-input";
+tempInput.name = "login-username";
+tempInput.type = "text";
+tempDiv.append(tempLabel);
+tempDiv.append(tempInput);
+loginSlider.append(tempDiv);
+//add password input field\
+tempDiv = document.createElement("div");
+tempDiv.className = "input-field";
+tempLabel = document.createElement("label");
+tempLabel.className = "input-name";
+tempLabel.innerText = "Password";
+tempInput = document.createElement("input");
+tempInput.className = "form-input";
+tempInput.name = "login-password";
+tempInput.type = "text";
+tempDiv.append(tempLabel);
+tempDiv.append(tempInput);
+loginSlider.append(tempDiv);
+
+
+
+const openRegistration = async () => {
     if(registerButtonPressed){
+        //this is where the fetch will be send 
+        //we if empty then close like below 
+        //if what is entered is incorrect 
+        //then we clear the fields
+        //then break without changing buttonpress 
         registerButtonPressed = false;
-        //sending login and close logic 
+        registrationSlider.remove();
+       
     }else{
         registerButtonPressed = true;
-        //need to build the HTML internals based on the partial and just add it
-}
-
-const openLogin = () => { 
-    console.log("openLogin\n")
-    if(loginButtonPressed){
-        console.log("openLogin 1 \n")
-        loginButtonPressed = false;
-        loginSlider.remove();
-        //sending login and close logic 
-    }else{
-        loginButtonPressed = true;
-        container.append(loginSlider)
-        //open the login panel
-
+        registerDiv.prepend(registrationSlider)
+        
     }
 }
 
+const openLogin = async () => { 
+    if(loginButtonPressed){
+         //this is where the fetch will be send 
+        //we if empty then close like below 
+        //if what is entered is incorrect 
+        //then we clear the fields
+        //then break without changing buttonpress 
+        loginButtonPressed = false;
+        loginSlider.remove();
+        
+    }else{
+        loginButtonPressed = true;
+        loginDiv.prepend(loginSlider)
+    }
+}
+
+const addRegistrationSlider = () => {
+    let registrationSlider = document.createElement("div");
+    registrationSlider.className = "slider-container";
+    registrationSlider.id = "registration-slider";
+    let tempDiv = document.createElement("div");
+    tempDiv.classList.add("auth-container");
+    registrationSlider.append(tempDiv);
+    //add username input field 
+    tempDiv = document.createElement("div");
+    tempDiv.className = "input-field";
+    let tempLabel = document.createElement("label");
+    tempLabel.className = "input-name";
+    tempLabel.innerText = "Username";
+    let tempInput = document.createElement("input");
+    tempInput.className = "form-input";
+    tempInput.name = "register-username";
+    tempInput.type = "text";
+    tempDiv.append(tempLabel);
+    tempDiv.append(tempInput);
+    registrationSlider.append(tempDiv);
+    //add password input field\
+    tempDiv = document.createElement("div");
+    tempDiv.className = "input-field";
+    tempLabel = document.createElement("label");
+    tempLabel.className = "input-name";
+    tempLabel.innerText = "Password";
+    tempInput = document.createElement("input");
+    tempInput.className = "form-input";
+    tempInput.name = "register-password";
+    tempInput.type = "text";
+    tempDiv.append(tempLabel);
+    tempDiv.append(tempInput);
+    registrationSlider.append(tempDiv);
+    //add confirm password input
+    tempDiv = document.createElement("div");
+    tempDiv.className = "input-field";
+    tempLabel = document.createElement("label");
+    tempLabel.className = "input-name";
+    tempLabel.innerText = "Confirm Password";
+    tempInput = document.createElement("input");
+    tempInput.className = "form-input";
+    tempInput.name = "register-confirm-password";
+    tempInput.type = "text";
+    tempDiv.append(tempLabel);
+    tempDiv.append(tempInput);
+    registrationSlider.append(tempDiv);
+    
+    registerDiv.prepend(registrationSlider);
+    
+}
 registerButton.addEventListener("click",openRegistration);
 
 loginButton.addEventListener("click", openLogin);
